f0dd039281a5ec59ccffe962804ce88630ed355d
shamacher <69808455+shamacher@users.noreply.github.com>
2020-08-19 13:22:56 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Sam Hamacher
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("Hello World!\n");
+
+    return 0;
+}
